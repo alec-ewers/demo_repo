cdd6d50542fdb1fd6cfcfb13fe6fb8b43655876a
mr_penumbra <mr_penumbra@Ravnen-PC>
2018-05-03 10:54:26 -0700

    This is an updated demo

--- src/jeebieheebies/JeebieHeebies.java
+++ src/jeebieheebies/JeebieHeebies.java
@@ -15,7 +15,9 @@ public class JeebieHeebies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("stuff and/or things");
+        String temp = "";
+        temp = "woooooooooooooooo!";
+        System.out.println(temp);
     }
     
 }
